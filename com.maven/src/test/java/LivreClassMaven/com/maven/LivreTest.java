30977695be2186d6b51a483565c81c1ceb0f6a42
Wafa Ismail <ismailwafa1004@gmail.com>
2017-01-21 13:15:19 -0500

    Update LivreTest.java

--- com.maven/src/test/java/LivreClassMaven/com/maven/LivreTest.java
+++ com.maven/src/test/java/LivreClassMaven/com/maven/LivreTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class LivreTest {
 	Livre livre = new Livre("Harry Potter " , "J.K. Rolling " , "Bloomsbury Publishing " , 26 , "roman");
-	Livre livre1 = new Livre("Harry Potter " , "J. Rolling " , "Bloomsbury Publishing " , 26 , "roman");
+	Livre livre1 = new Livre("Harry Potter " , "J.K. Rolling " , "Bloomsbury Publishing " , 26 , "roman");
 	
 	@Test
 	public void testEqualsLivre() {
